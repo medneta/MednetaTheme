b44ea8505f4e2849e1326e47d734f70ece970bf9
Achilleas Pipinellis <axilleas@axilleas.me>
2015-10-18 16:39:21 +0300

    Init commit

---
+++ js/script.js
@@ -0,0 +1,63 @@
+
+var urlOverview="http://medneta.eu/project-overview";
+var urlMethodology="http://medneta.eu/methodology";
+var urlPartnership = "http://medneta.eu/partnership";
+
+
+$(document).ready(function(){
+
+		$("#block-block-6").click(function(e){
+			window.location.href = urlOverview;
+		});
+		
+		
+		$("#block-block-16").click(function(e){
+			window.location.href = urlOverview;
+		});
+		
+		
+		
+		$("#block-block-15").click(function(e){
+			window.location.href = urlMethodology;
+		});
+		
+		$("#block-block-7").click(function(e){
+			window.location.href = urlMethodology;
+		});
+		
+		
+		$("#block-block-8").click(function(e){
+			window.location.href = urlPartnership;
+		});
+		
+		$("#main-menu").append('<li class="menu-345"><a href="http://webmail.medneta.eu" target="_blank" data-thmr="thmr_28">WEB MAIL</a></li>');
+		
+		
+	
+	
+	
+	$("#pikame0").PikaChoose({carousel:true,carouselOptions:{wrap:'circular'}});               
+	$("#pikame1").PikaChoose({carousel:true,carouselOptions:{wrap:'circular'}});               
+	$("#pikame2").PikaChoose({carousel:true,carouselOptions:{wrap:'circular'}});           
+	$("#pikame3").PikaChoose({carousel:true,carouselOptions:{wrap:'circular'}});             
+	$("#pikame4").PikaChoose({carousel:true,carouselOptions:{wrap:'circular'}});               
+	$("#pikame5").PikaChoose({carousel:true,carouselOptions:{wrap:'circular'}});                
+	$("#pikame6").PikaChoose({carousel:true,carouselOptions:{wrap:'circular'}});                
+	$("#pikame7").PikaChoose({carousel:true,carouselOptions:{wrap:'circular'}});                
+	$("#pikame8").PikaChoose({carousel:true,carouselOptions:{wrap:'circular'}});                 
+	$("#pikame9").PikaChoose({carousel:true,carouselOptions:{wrap:'circular'}});                 
+	$("#pikame10").PikaChoose({carousel:true,carouselOptions:{wrap:'circular'}});                 
+	$("#pikame11").PikaChoose({carousel:true,carouselOptions:{wrap:'circular'}});                 
+	$("#pikame12").PikaChoose({carousel:true,carouselOptions:{wrap:'circular'}});                 
+	$("#pikame13").PikaChoose({carousel:true,carouselOptions:{wrap:'circular'}});                 
+	$("#pikame14").PikaChoose({carousel:true,carouselOptions:{wrap:'circular'}});    
+	
+	
+		
+});
+
+
+
+
+
+
